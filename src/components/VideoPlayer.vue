d0be2013c747e13b96874ef3aa73ec9980b1db74
rxb3rth <reliutg@gmail.com>
2023-03-05 17:06:10 +0100

    feat: use cloudinary-video-player

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,38 +1,46 @@
 <script>
-import videojs from 'video.js';
-import 'video.js/dist/video-js.css'
+import { videoPlayer } from "cloudinary-video-player";
+import "cloudinary-video-player/cld-video-player.min.css";
 
 export default {
-    name: 'VideoPlayer',
-    props: {
-        options: {
-            type: Object,
-            default() {
-                return {};
-            }
-        }
+  name: "VideoPlayer",
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {};
+      },
     },
-    data() {
-        return {
-            player: null
-        }
-    },
-    mounted() {
-        this.player = videojs(this.$refs.videoPlayer, this.options, () => {
-            this.player.log('onPlayerReady', this);
-        });
-    },
-    beforeUnmount() {
-        if (this.player) {
-            this.player.dispose();
-        }
-    }
-}
+  },
+  data() {
+    return {
+      player: null,
+    };
+  },
+  mounted() {
+    this.player = videoPlayer("doc-player", {
+      cloud_name: "roberthgnz",
+    });
+    this.player.source(this.options.source, {
+      sourceTypes: ["hls"],
+      transformation: {
+        streaming_profile: "hd",
+      },
+    });
+  },
+};
 </script>
 
 <template>
-  <video
-    ref="videoPlayer"
-    class="video-js"
-  />
+  <div class="max-w-[800px]">
+    <video id="doc-player" controls muted class="cld-video-player cld-fluid">
+      <track
+        :kind="options.track.kind"
+        :src="options.track.url"
+        :srclang="options.track.srclang"
+        :label="options.track.label"
+        default
+      />
+    </video>
+  </div>
 </template>
